f9bb785ba4cb89bf7e2afae16b6954d742fd8e70
Rishikesh Fanse <rishikeshfanse@gmail.com>
2015-03-27 21:01:35 +0530

    android with web services

--- Sher-Lock_Remote/app/src/main/java/com/srp/sher_lock_remote/MainActivity.java
+++ Sher-Lock_Remote/app/src/main/java/com/srp/sher_lock_remote/MainActivity.java
@@ -1,12 +1,23 @@
 package com.srp.sher_lock_remote;
 
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -17,28 +28,106 @@ import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
-
+/*
     TextView chat;
     EditText msg;
     Socket socket;
     BufferedReader in;
     InetAddress servraddr;
+    **/
+    String resp, resp1;
+    Integer flag;
+    protected static String url = "http://rishikesh.byethost5.com//error.php";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        chat = (TextView) findViewById(R.id.txtChat);
-        msg= (EditText) findViewById(R.id.txtMsg);
+    //    chat = (TextView) findViewById(R.id.txtChat);
+    //    msg= (EditText) findViewById(R.id.txtMsg);
+    //    new Thread(new ClientThread()).start();
+        final Button shutdown = (Button) findViewById(R.id.shutdown);
+        shutdown.setOnClickListener
+                (
+                        new Button.OnClickListener() {
+                            public void onClick(View v) {
+                                flag=1;
+                                new putcall().execute();
 
+                            }
+                        }
+                );
 
-        new Thread(new ClientThread()).start();
+        final Button screenshot = (Button) findViewById(R.id.screenshot);
+        screenshot.setOnClickListener
+                (
+                        new Button.OnClickListener() {
+                            public void onClick(View v) {
+                                flag=2;
+                                new putcall().execute();
+
+                            }
+                        }
+                );
+
 
     }
+    private class putcall extends AsyncTask<Void, Void, Void> {
+        protected void onPreExecute() {
+            Toast.makeText(getApplicationContext(), "Starting",
+                    Toast.LENGTH_LONG).show();
+        }
 
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpPost httppost = new HttpPost(url);
+                System.out.println("hello");
+                try {
+
+                    List<NameValuePair> data = new ArrayList<NameValuePair>(1);
+                    if (flag == 1)
+                        data.add(new BasicNameValuePair("shutdown", "shutdown"));
+                    else if (flag == 2)
+                        data.add(new BasicNameValuePair("screenshot", "screenshot"));
+
+                    httppost.setEntity(new UrlEncodedFormEntity(data));
+                    System.out.println("" + data);
+
+                    HttpResponse rs = httpclient.execute(httppost);
+
+                    System.out.println("hello");
+
+
+                } catch (Exception e) {
+                    System.out.println("kay ahhe he");
+
+                    e.printStackTrace();
+                }
+            } catch (Exception e) {
+                System.out.println("kay nahi");
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+
+        protected void onPostExecute(Void void1) {
+            System.out.println("coming 3");
+
+
+            Toast.makeText(getApplicationContext(), "Review given", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+/*
     class ClientThread implements Runnable
     {
 
@@ -55,31 +144,9 @@ public class MainActivity extends ActionBarActivity {
             catch(Exception e){}
         }
     }
+*/
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-
+/*
      public void send(View view)
     {
 
@@ -94,4 +161,5 @@ public class MainActivity extends ActionBarActivity {
         }
         msg.setText("");
     }
+    */
 }
